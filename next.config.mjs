f5e2c4ce3377b7475082c90f5ee4ab5bb82b5e81
Sanjalionething <sanjalisahu@onething.design>
2024-12-04 15:50:24 +0530

    changed gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export", // Enables static export
-  basePath: "/internal-project-nextjs", // Set the base path to match your GitHub repository name
-  assetPrefix: "/internal-project-nextjs/", // Prefix assets with the base path
-};
+const nextConfig = {};
 
 export default nextConfig;
