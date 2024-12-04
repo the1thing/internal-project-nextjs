9106ca1e4dec0518b556d190038b58c72f6acfbf
Sanjalionething <sanjalisahu@onething.design>
2024-12-04 14:17:17 +0530

    second commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export", // Enables static export
+  basePath: "/internal-project-nextjs", // Set the base path to match your GitHub repository name
+  assetPrefix: "/internal-project-nextjs/", // Prefix assets with the base path
+};
 
 export default nextConfig;
